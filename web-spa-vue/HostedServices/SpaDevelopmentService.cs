8961d62bddb5f4279bf72c1421b8afbba1dd6a60
Hannes Zbinden <hanneszbinden@outlook.com>
2022-02-01 17:52:38 +0100

    wip.

---
+++ web-spa-vue/HostedServices/SpaDevelopmentService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+namespace web_spa_vue.HostedServices
+{
+  public class SpaDevelopmentService : IHostedService
+  {
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
