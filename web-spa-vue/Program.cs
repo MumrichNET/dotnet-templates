8a688a7bf25ad08c281f4fec9f34b0fac7739768
Hannes Zbinden <hanneszbinden@outlook.com>
2022-02-01 17:53:45 +0100

    init hosted-service.

--- web-spa-vue/Program.cs
+++ web-spa-vue/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+using web_spa_vue.HostedServices;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostedService<SpaDevelopmentService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
