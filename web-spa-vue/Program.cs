c9b5b4d763c9db27aa41d41e1c97739be1176470
Hannes Zbinden <hanneszbinden@outlook.com>
2022-02-10 18:18:46 +0100

    fully working DEBUG- & RELEASE-Profiles.

--- web-spa-vue/Program.cs
+++ web-spa-vue/Program.cs
@@ -1,6 +1,10 @@
+using System.IO;
+
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 using SpaDevServer.HostedServices;
 
@@ -10,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 var appSettings = builder.Configuration.Get<AppSettings>();
 
 builder.Services.AddSingleton(appSettings);
+builder.Services.AddControllersWithViews();
 
 #if DEBUG
 builder.Services.AddHostedService<ViteJsDevelopmentService>();
@@ -18,9 +23,29 @@ builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSecti
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
+#if RELEASE
+var clientAppRoot = Path.Combine(builder.Environment.ContentRootPath, "client-app/dist");
+app.UseStaticFiles(new StaticFileOptions
+{
+  FileProvider = new PhysicalFileProvider(clientAppRoot),
+  RequestPath = ""
+});
+#endif
+
 app.UseRouting();
+
 #if DEBUG
 app.MapReverseProxy();
+#endif
+
+app.MapControllers();
+app.MapDefaultControllerRoute();
+
+#if RELEASE
+var clientAppIndex = Path.Combine(clientAppRoot, "index.html");
+app.MapGet("/", async context => await context.Response.SendFileAsync(clientAppIndex));
 #endif
 
 app.Run();
--- web-spa-vue/client-app/src/components/HelloWorld.vue
+++ web-spa-vue/client-app/src/components/HelloWorld.vue
@@ -28,7 +28,7 @@ const count = ref(0)
 
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
-    Edit
+    Edit :-)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
