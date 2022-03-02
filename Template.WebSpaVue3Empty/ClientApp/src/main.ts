362f91820c9667e0c76f971d202ee6e02c509ff3
Hannes Zbinden <hanneszbinden@outlook.com>
2022-03-02 21:00:42 +0100

    add spa.

---
+++ Template.WebSpaVue3Empty/ClientApp/src/main.ts
@@ -0,0 +1,9 @@
+import "virtual:windi.css";
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.mount("#app");
