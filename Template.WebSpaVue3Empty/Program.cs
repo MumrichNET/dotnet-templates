8f292aa8d9b6bdf7d57a39755f8737fadbe9387c
Hannes <hanneszbinden@outlook.com>
2022-09-02 01:03:16 +0200

    wip.

--- Template.WebSpaVue3Empty/Program.cs
+++ Template.WebSpaVue3Empty/Program.cs
@@ -1,25 +1,38 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Mumrich.SpaDevMiddleware;
+using Mumrich.SpaDevMiddleware.Contracts;
 using Mumrich.SpaDevMiddleware.Extensions;
 
-using Template.WebSpaVue3Empty;
+namespace Template.WebSpaVue3Empty;
 
-var builder = WebApplication.CreateBuilder(args);
-var appSettings = builder.Configuration.Get<AppSettings>();
+public class AppSettings : ISpaDevServerSettings
+{
+    public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new();
+}
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddCors();
-builder.RegisterSinglePageAppDevMiddleware(appSettings);
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var appSettings = builder.Configuration.Get<AppSettings>();
 
-var app = builder.Build();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddCors();
+        builder.RegisterSinglePageAppDevMiddleware(appSettings);
 
-app.UseStaticFiles();
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseCors();
-app.MapControllers();
-app.MapSinglePageApps(appSettings);
+        var app = builder.Build();
 
-app.Run();
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseCors();
+        app.MapControllers();
+        app.MapSinglePageApps(appSettings);
+
+        app.Run();
+    }
+}
