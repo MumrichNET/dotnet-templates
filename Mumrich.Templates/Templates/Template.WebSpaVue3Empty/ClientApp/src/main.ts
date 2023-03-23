c798a7519be747a34ed12cda8c1dc648fbdf7e31
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-03-24 00:02:59 +0100

    use new vue-template.

--- Mumrich.Templates/Templates/Template.WebSpaVue3Empty/ClientApp/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "virtual:windi.css";
-import App from "./App.vue";
-import {createApp} from "vue";
-import {createPinia} from "pinia";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.mount("#app");
