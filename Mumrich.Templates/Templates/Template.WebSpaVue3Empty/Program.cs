d59f59aef18339fce5d153ae4ab3fe4f5ef70404
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-03-24 04:19:45 +0100

    update packages and templates.

--- Mumrich.Templates/Templates/Template.WebSpaVue3Empty/Program.cs
+++ Mumrich.Templates/Templates/Template.WebSpaVue3Empty/Program.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using Mumrich.SpaDevMiddleware.Contracts;
+using Mumrich.SpaDevMiddleware.Domain.Contracts;
+using Mumrich.SpaDevMiddleware.Domain.Models;
 using Mumrich.SpaDevMiddleware.Extensions;
-using Mumrich.SpaDevMiddleware.Models;
 
 namespace Template.WebSpaVue3Empty;
 
@@ -15,6 +15,7 @@ public class AppSettings : ISpaDevServerSettings
 {
   public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new();
   public string SpaRootPath { get; set; } = Directory.GetCurrentDirectory();
+  public bool UseParentObserverServiceOnWindows { get; set; } = true;
 }
 
 public static class Program
